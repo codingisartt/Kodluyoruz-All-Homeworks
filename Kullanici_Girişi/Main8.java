e25f4a318c91f82e1ac9d8f4f63e8f271f37fcb9
Tubanur Balci <tubanurbalci@gmail.com>
2021-11-17 16:24:41 +0300

    Main8.java is added

--- Kullanici_Girişi/Main8.java
+++ Kullanici_Girişi/Main8.java
@@ -2,31 +2,41 @@ import java.util.Scanner;
 
 public class Main8{
     public static void main(String[] args) {
-        String userName, password;
+        String userName="tubanur";
+        var password="123456";
+        int click;
 
-        Scanner input=new Scanner(System.in);
-        System.out.print("Kullanıcı adı giriniz: ");
-        userName=input.nextLine();
-        System.out.print("Şifre giriniz: ");
-        password=input.nextLine();
+        Scanner inp =new Scanner(System.in);
+        System.out.print("Kullanıcı adını giriniz: ");
+        userName=inp.nextLine();
+        System.out.print("Şifreyi giriniz: ");
+        password=inp.nextLine();
+        if(!"tubanur".equals(userName) && !"123456".equals(password)){
+            System.out.println("Hem kullanıcı adınız hem şifreniz yanlış !");
+        }else if(!"tubanur".equals(userName)){
+            System.out.println("Kullanıcı adınız yanlış ! ");
+        }else if(!"123456".equals(password )){
+            System.out.print("Şifreniz Yanlış ! Şifrenizi değiştirmek için 1'e çıkış yapmak için lütfen 2'ye basınız: ");
+            click=inp.nextInt();
+            if(click==1){
+                System.out.print("Eski şifreden farklı bir şifre oluşturun: ");
+                password=inp.next();
+                if(!"123456".equals(password)){
+                    System.out.println("Şifre başarıyla oluşturuldu.");
+                }else{
+                    System.out.println("Bu şifre kullanılamaz !");
+                }
 
-        switch(userName){
-            case "codingisartt":
-                System.out.println("Kullanıcı adı doğru...");
-                break;
-            default: System.out.println("Yanlış kullanıcı adı!!");     
+            }else if(click==2){
+                System.out.println("Çıkış yapılıyor...");
+            }else{
+                System.out.println("Yanlış sayı girdiniz...");
+            }
+        }else{
+            System.out.println("Giriş başarılı..");
         }
-        switch(password){
-            case "tuba123":
-                System.out.println("Parola doğru...");
-                break;
-            default: System.out.println("Yanlış şifre!!");
-                
 
-        }
-        
 
 
     }
-
 }
